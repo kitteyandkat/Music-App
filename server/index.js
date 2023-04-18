58bcdc08fdd0c6564c04c96e6dde5c3bd4533a0f
Sarah Johnson <kitteyandkat@aim.com>
2023-04-18 11:40:42 -0400

    SERVE KWEEN YAASSS

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import express from 'express'
 // import dotenv from 'dotenv'
 import cors from 'cors'
 import read from './queries/read.js'
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
 
 const app = express();
@@ -13,12 +15,16 @@ app.use(cors({
   origin: '*'
 }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// });
 
 app.get('/artists', read.artists)
 app.get('/songs', read.songs)
+
+//Serve any file in the build folder
+const _dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(_dirname, '..', 'client', 'dist')));
 
 app.listen(port,() => {
   console.log(`Server Listening at http://localhost:${port}`);
